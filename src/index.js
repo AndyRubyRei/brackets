67fc662091399d6607d940cf9d928175c886d24b
Nargiza Dzh <nargizadzh@gmail.com>
2023-10-30 00:54:46 +0600

    feat: problem solving

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
+  const openingBrackets = bracketsConfig.map(([opening]) => opening);
+  const closingBrackets = bracketsConfig.map(([, closing]) => closing);
+
   // your solution
-}
+  const stack = [];
+  for (let i = 0; i < str.length; i++) {
+    const bracket = str[i];
+    const correspondingOpeningBracketIdx = openingBrackets.indexOf(bracket);
+    const correspondingClosingBracketIdx = closingBrackets.indexOf(bracket);
+    const isOpeningBracket = correspondingOpeningBracketIdx !== -1;
+    const isClosingBracket = correspondingClosingBracketIdx !== -1;
+
+    if (correspondingOpeningBracketIdx === correspondingClosingBracketIdx) {
+      if (stack.length && stack[stack.length - 1] === bracket) {
+        stack.pop();
+      } else {
+        stack.push(bracket);
+      }
+    } else if (isOpeningBracket) {
+      stack.push(bracket);
+    } else if (isClosingBracket) {
+      if (openingBrackets[correspondingClosingBracketIdx] !== stack.pop()) {
+        return false;
+      }
+    }
+  }
+
+  if (stack.length) {
+    return false;
+  } else {
+    return true;
+  }
+};
